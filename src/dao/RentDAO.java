ea4730e252c6ef0f9683c32d0e07dadbadf4662d
Isaac Reis <isaacreis2807@gmail.com>
2026-04-14 21:50:26 -0300

    feat: implementation of the RentDAO class

--- src/dao/RentDAO.java
+++ src/dao/RentDAO.java
@@ -1,4 +1,81 @@
 package dao;
 
+import connection.ConnectionFactory;
+
+import java.sql.*;
+import java.time.LocalDate;
+
 public class RentDAO {
+
+    public void rentVehicle(Long id, String clientName, LocalDate startDate) {
+
+        PreparedStatement stmt;
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+
+            stmt = conn.prepareStatement("UPDATE vehicles SET available = false WHERE id = ?");
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+
+            stmt = conn.prepareStatement("INSERT INTO rent (vehicle_id, client_name, start_date) VALUES (?, ?, ?)");
+            stmt.setLong(1, id);
+            stmt.setString(2, clientName);
+            stmt.setDate(3, Date.valueOf(startDate));
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void returnVehicle(Long id) {
+
+        PreparedStatement stmt;
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+
+            stmt = conn.prepareStatement("UPDATE vehicles SET available = true WHERE id = ?");
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+
+            stmt = conn.prepareStatement("UPDATE rent SET finished = true WHERE vehicle_id = ?");
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String listAllRents() {
+
+        PreparedStatement stmt;
+        String sql = "SELECT * FROM rent";
+        String rows;
+        String table = "";
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+
+            stmt = conn.prepareStatement(sql);
+            ResultSet result = stmt.executeQuery();
+
+            while (result.next()) {
+                Long id = result.getLong("id");
+                Long vehicleId = result.getLong("vehicle_id");
+                String clientName = result.getString("client_name");
+                LocalDate startDate = result.getDate("start_date").toLocalDate();
+                LocalDate endDate = result.getDate("end_date").toLocalDate();
+                boolean finished = result.getBoolean("finished");
+
+                rows = id + " | " + vehicleId + " | " + clientName + " | " +
+                        startDate + " | " + endDate + " | " + finished +  " | " + "\n";
+                table += rows;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return table;
+    }
 }
